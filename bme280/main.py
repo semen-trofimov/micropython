471ad2c6f089563d21c97da10dd642a304acaf3d
semen.trofimov <semiony@gmail.com>
2022-01-07 17:51:24 +0300

    add bme280

--- bme280/main.py
+++ bme280/main.py
@@ -1,18 +1,19 @@
-# https://microcontrollerslab.com/micropython-bme280-esp32-esp8266-temperature-humidity-pressure/
-
-from machine import Pin, I2C        #importing relevant modules & classes
+from machine import Pin, I2C
 from time import sleep
-import BME280        #importing BME280 library
+import BME280
 
-i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)     #initializing the I2C method for ESP32
-#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)       #initializing the I2C method for ESP8266
+# ESP32 - Pin assignment
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
+# ESP8266 - Pin assignment
+#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 
 while True:
-    bme = BME280.BME280(i2c=i2c)          #BME280 object created
-    temperature = bme.temperature            #reading the value of temperature
-    humidity = bme.humidity                     #reading the value of humidity
-    pressure = bme.pressure
-    print('Temperature is: ', temperature)    #printing BME280 values
-    print('Humidity is: ', humidity)
-    print('Pressure is: ', pressure)
-    sleep(10)           #delay of 10s                   #reading the value of pressure
+    bme = BME280.BME280(i2c=i2c)
+    temp = bme.temperature
+    hum = bme.humidity
+    pres = bme.pressure
+    print('Temperature: ', temp)
+    print('Humidity: ', hum)
+    print('Pressure: ', pres)
+
+    sleep(5)
